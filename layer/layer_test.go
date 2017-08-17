1aa57ead5d3425f84699279e5a930a8f8b27c7f1
Sunjay Bhatia <sbhatia@pivotal.io>
2017-08-17 09:41:37 -0400

    WIP
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- layer/layer_test.go
+++ layer/layer_test.go
@@ -1,69 +1,55 @@
-package hcs_test
+package layer_test
 
 import (
-	"encoding/json"
 	"errors"
 	"io/ioutil"
-	"math/rand"
 	"os"
-	"path/filepath"
-	"strconv"
-	"time"
 
-	"code.cloudfoundry.org/winc/hcs"
+	"code.cloudfoundry.org/winc/layer"
+	"code.cloudfoundry.org/winc/layer/layerfakes"
 
 	"github.com/Microsoft/hcsshim"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Client", func() {
+var _ = Describe("Manager", func() {
 	var (
-		depot         string
-		containerId   string
-		driverInfo    hcsshim.DriverInfo
-		sandboxLayers []string
-		client        hcs.Client
+		client       *layerfakes.FakeHCSClient
+		m            *layer.Manager
+		parentLayers []string
+		storeDir     string
+		err          error
+		containerId  string
 	)
 
+	const expectedVolumeGuid = `\\?\Volume{some-guid}\`
+
 	BeforeEach(func() {
-		var err error
-		depot, err = ioutil.TempDir("", "")
+		storeDir, err = ioutil.TempDir("", "layer-store-dir")
 		Expect(err).NotTo(HaveOccurred())
 
-		driverInfo = hcsshim.DriverInfo{
-			Flavour: 1,
-			HomeDir: depot,
-		}
-		rand.Seed(time.Now().UnixNano())
-		containerId = strconv.Itoa(rand.Int())
+		parentLayers = []string{"layer-2", "layer-1"}
 
-		parentLayerChain, err := ioutil.ReadFile(filepath.Join(rootfsPath, "layerchain.json"))
-		Expect(err).NotTo(HaveOccurred())
-		parentLayers := []string{}
-		Expect(json.Unmarshal(parentLayerChain, &parentLayers)).To(Succeed())
-
-		sandboxLayers = append([]string{rootfsPath}, parentLayers...)
-
-		client = hcs.Client{}
+		client = &layerfakes.FakeHCSClient{}
+		m = layer.NewManager(client, storeDir)
 	})
 
 	AfterEach(func() {
-		Expect(os.RemoveAll(depot)).To(Succeed())
+		Expect(os.RemoveAll(storeDir)).To(Succeed())
 	})
 
 	Describe("CreateLayer", func() {
-		AfterEach(func() {
-			Expect(client.DestroyLayer(driverInfo, containerId)).To(Succeed())
+		BeforeEach(func() {
+			client.GetLayerMountPathReturns(expectedVolumeGuid, nil)
 		})
 
-		FIt("creates the layer", func() {
+		It("creates the layer", func() {
 			volumeGuid, err := client.CreateLayer(driverInfo, containerId, rootfsPath, sandboxLayers)
 			Expect(err).ToNot(HaveOccurred())
 
-			expectedVolumeGuid, err := hcsshim.GetLayerMountPath(driverInfo, containerId)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(volumeGuid).ToNot(BeEmpty())
+			Expect
+
 			Expect(volumeGuid).To(Equal(expectedVolumeGuid))
 		})
 
